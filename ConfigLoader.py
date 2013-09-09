b1f77f636ffd1bfcd41dea1e13fb436dbfcdb656
Kevin Baragona <kevin@Kevins-Mac-Pro.local>
2013-09-08 18:14:27 -0700

    fixing your mess

--- ConfigLoader.py
+++ ConfigLoader.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+
 import ConfigParser
 Config = ConfigParser.ConfigParser()
 Config
 Config.read("jooblerconfig.ini")
-Config.sections()
-['Default]]
+for section in Config.sections():
+    print section+':'
+    for option in Config.options(section):
+        print '\t'+option+':'+Config.get(section,option)
+
