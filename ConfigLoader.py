44f9d871eee2671e78a1d7cbd34f2e080c0d9571
Kevin Baragona <kevin@Kevins-Mac-Pro.local>
2013-09-14 18:48:23 -0700

    switching to JSON for configs

--- ConfigLoader.py
+++ ConfigLoader.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python
 
-import ConfigParser
-Config = ConfigParser.ConfigParser()
-Config
-Config.read("jooblerconfig.ini")
-for section in Config.sections():
-    print section+':'
-    for option in Config.options(section):
-        print '\t'+option+':'+Config.get(section,option)
+import json
+import pprint
 
+def getConfig():
+    defaults = {
+        'LibraryRoots':[],
+        'VideoFileExtensions':[],
+        'AudioFileExtensions':[],
+        'MaxSearchDepth':[],
+        'MinMovieSize':[],
+        'MinAudioSize':[],
+
+    }
+    cfg={}
+    try:
+        file = open('joobler_config.json')
+        cfg = json.load(file)
+        file.close()
+    except:
+        print 'couldnt open config file'
+    
+
+    for key in defaults:
+        if not key in cfg:
+            cfg[key] = defaults[key]
+            print 'using defaults for config key '+key
+    return cfg
+    
+def saveConfig(cfg):
+    file = open('joobler_config.json','w')
+    json.dump(cfg,file,indent=4)
+    file.close()
+    
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(getConfig())
+
+saveConfig(getConfig())
